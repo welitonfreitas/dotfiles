93aacee5a4ca66d2c77e11930afb4b157393d499
Weliton Freitas <notilew@gmail.com>
2025-06-07 14:04:53 -0300

    adiciona configuração de temas para Neovim

---
+++ .config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,63 @@
+return {
+  -- the colorscheme should be available when starting Neovim
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("tokyonight").setup({
+        on_highlights = function(hl, c)
+          local prompt = "#2d3149"
+          hl.TelescopeNormal = {
+            bg = c.bg_dark,
+            fg = c.fg_dark,
+          }
+          hl.TelescopeBorder = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.TelescopePromptNormal = {
+            bg = prompt,
+          }
+          hl.TelescopePromptBorder = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.TelescopePromptTitle = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.TelescopePreviewTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.TelescopeResultsTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+        end,
+      })
+      vim.cmd([[colorscheme tokyonight-night]])
+    end,
+  },
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('rose-pine').setup({
+        variant = 'moon',
+        disable_background = true,
+        disable_float_background = true,
+        disable_italics = true,
+      })
+    end,
+  },
+  { 
+    "catppuccin/nvim", 
+    name = "catppuccin", 
+    priority = 1000 
+  }
+
+}
